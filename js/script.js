f229790901de14aed4c1ad5305c4c685c5d6cde4
Shivani Agrawal <shivagra1908@gmail.com>
2017-04-24 11:32:05 +0530

    responsive

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ $('.taskinput').keypress(function (e) {
  var key = e.which;
  if(key == 13)  // the enter key code
   {
-    var answer = $("input[name=input-text]").val();
-    var currentDiv = $('<div class="col-sm-4 item">' + answer + '</div>')
+    var answer = $(".taskinput").val();
+    var currentDiv = $('<div class="col-sm-6"><div class="item">' + answer + '</div></div>')
     $('.tasks').append(currentDiv);
+    $(".taskinput").val('');
   }
 var safeColors = ['00','33','66','99','cc','ff'];
 var rand = function() {
@@ -17,4 +18,5 @@ var randomColor = function() {
     return "#"+r+g+b;
 };
 $(currentDiv).css('background',randomColor());
+
 });
